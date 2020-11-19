f2203ee98400dde80cda9f0ec35e4516517a0496
Eduardo <eduardobobato@hotmailcom.br>
2020-11-19 00:04:10 -0300

    Implementando list e create

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: "income" | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((acc, obj) => {
+      if (obj.type === 'income') {
+        acc.income += obj.value;
+        acc.total += obj.value;
+      }
+      if (obj.type === 'outcome') {
+        acc.outcome += obj.value;
+        acc.total -= obj.value;
+      }
+      return acc;
+    },{
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
